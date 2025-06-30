2d4bee6237f9bd55f7aad0d74588f5dbd3d0941d
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-30 18:20:55 +0200

    feat: Add multer dependency and update app module imports for file handling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MinioModule } from './minio/minio.module';
+import { ConfigModule } from '@nestjs/config';
+import { FilesModule } from './files/files.module'; // Импортируем FilesModule
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    MinioModule,
+    FilesModule, // Добавляем FilesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
